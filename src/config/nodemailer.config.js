cacb02b66ee72fe6e7693e7a6e19653ee25804e5
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-18 23:05:18 +0600

    create nodemailer.config.js file and configure nodemailer

---
+++ src/config/nodemailer.config.js
@@ -0,0 +1,15 @@
+// import modules ---------------------------------------->
+import nodemailer from 'nodemailer';
+
+// transporter ------------------------------------------->
+const transporter = nodemailer.createTransport({
+    host: 'smtp-relay.brevo.com',
+    port: 587,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+    },
+});
+
+// export modules ---------------------------------------->
+export default transporter;
