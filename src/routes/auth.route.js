acd9c04c4a3f44b31156196babee61f135cf391f
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-18 21:12:36 +0600

    add new sign-in route with sign-in controller

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,12 +1,14 @@
 // import modules ---------------------------------------->
 import express from 'express';
 import authSignupController from '../controllers/auth.signup.controller.js';
+import authSignInController from '../controllers/auth.signin.controller.js';
 
 // router ------------------------------------------------>
 const router = express.Router();
 
 // route ------------------------------------------------->
 router.route('/sign_up').post(authSignupController);
+router.route('/sign_in').post(authSignInController);
 
 // export modules ---------------------------------------->
 export default router;
