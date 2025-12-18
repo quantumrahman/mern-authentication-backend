93829e02a68af01835aa05079085f794492b04da
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-18 21:41:31 +0600

    add new sign-out route in auth.route.js file

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import authSignupController from '../controllers/auth.signup.controller.js';
 import authSignInController from '../controllers/auth.signin.controller.js';
+import authSignOutController from '../controllers/auth.signout.controller.js';
 
 // router ------------------------------------------------>
 const router = express.Router();
@@ -9,6 +10,7 @@ const router = express.Router();
 // route ------------------------------------------------->
 router.route('/sign_up').post(authSignupController);
 router.route('/sign_in').post(authSignInController);
+router.route('/sign_out').get(authSignOutController);
 
 // export modules ---------------------------------------->
 export default router;
