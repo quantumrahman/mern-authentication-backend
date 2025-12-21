bb9ebeda375da7eeb9f12c81db1e344bd82c3272
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-21 12:01:35 +0600

    add two new api end points

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,11 @@
 // import modules ---------------------------------------->
 import express from 'express';
+import authMiddleware from '../middlewares/auth.middleware.js';
 import authSignupController from '../controllers/auth.signup.controller.js';
 import authSignInController from '../controllers/auth.signin.controller.js';
 import authSignOutController from '../controllers/auth.signout.controller.js';
+import authVerificationOtpController from '../controllers/auth.verificationOtp.controller.js';
+import authVerifyOtpController from '../controllers/auth.verifyOtp.controller.js';
 
 // router ------------------------------------------------>
 const router = express.Router();
@@ -11,6 +14,11 @@ const router = express.Router();
 router.route('/sign_up').post(authSignupController);
 router.route('/sign_in').post(authSignInController);
 router.route('/sign_out').get(authSignOutController);
-
+router
+    .route('/verification_otp')
+    .post(authMiddleware, authVerificationOtpController);
+router
+    .route('/verify_email_account')
+    .post(authMiddleware, authVerifyOtpController);
 // export modules ---------------------------------------->
 export default router;
